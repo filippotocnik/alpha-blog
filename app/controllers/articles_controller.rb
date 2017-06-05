2053447029d7b5e4ecd32b987e72ac2ec595458e
Filip <filip.potocnik@me.com>
2017-06-05 13:41:26 +0200

    article edited completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,18 +4,32 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
-      render 'new'  
+      render 'new'
     end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -24,7 +38,7 @@ class ArticlesController < ApplicationController
   end
 
   #def articles_show
-    
+
   #end
 
 end
