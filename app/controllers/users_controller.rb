dfab10244e334583279200b1c5ee303866196f9b
Filip <filip.potocnik@me.com>
2017-06-09 10:56:21 +0200

    created user article page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,15 @@ class UsersController < ApplicationController
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-        flash[:success] = 'your account was updates successfully'
+        flash[:success] = 'your account was updated successfully'
         redirect_to articles_path
       else
         render 'edit'
       end
+    end
+
+    def show
+      @user = User.find(params[:id])
     end
   end
 
