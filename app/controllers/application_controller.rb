1464c03c6a38db25598f76ae18a0086cb1e18d41
Filip <filip.potocnik@me.com>
2017-06-16 10:26:38 +0200

    complete authentication sistem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    if !logged_in
+    if !logged_in?
       flash[:danger] = 'you must be logged in...'
       redirect_to root_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -46,5 +47,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = 'you can only edit your own account'
+      redirect_to root_path
+    end
   end
 end
